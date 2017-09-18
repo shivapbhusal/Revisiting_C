b49bf72809af4524dcd7815cb1a1dcfbd2bf3cc6
shivapbhusal <civabhusal@gmail.com>
2017-09-18 16:09:49 -0400

    buffer overflow example added

---
+++ BufferOverFlow/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char buff[15];
+    int pass = 0;
+
+    printf("\n Enter the password : \n");
+    gets(buff);
+
+    if(strcmp(buff, "thegeekstuff"))
+    {
+        printf ("\n Wrong Password \n");
+    }
+    else
+    {
+        printf ("\n Correct Password \n");
+        pass = 1;
+    }
+
+    if(pass)
+    {
+       /* Now Give root rights to user*/
+        printf ("\n Root privileges given to the user \n");
+    }
+    else {
+        printf ("\n Root privileges not given to user\n")
+    }
+
+    return 0;
+}
